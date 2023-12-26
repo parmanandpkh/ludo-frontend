84c3addbc181ed9f504fbe9b1a010c21249863d3
Dharmendra singh <dharmendra.shekhawat@devtechnosys.info>
2023-12-26 19:16:49 +0530

    changes in dashboard

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -11,7 +11,7 @@ export default function DashboardAppPage() {
     <>
       <Container maxWidth="xl">
         <Typography variant="h4" sx={{ mb: 5 }}>
-          Hi, Welcome back
+          Hi, Welcome 
         </Typography>
 
         {/* <Grid container spacing={3}>
